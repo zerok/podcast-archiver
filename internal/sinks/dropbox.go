198d7e23aafbb3e971b199658a69998e56d4faba
Horst Gutmann <zerok@zerokspot.com>
2024-01-06 10:39:34 +0100

    chore: Update dropbox sdk (#8)

--- internal/sinks/dropbox.go
+++ internal/sinks/dropbox.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,8 +19,8 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox"
-	dfiles "github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files"
+	"github.com/dropbox/dropbox-sdk-go-unofficial/v6/dropbox"
+	dfiles "github.com/dropbox/dropbox-sdk-go-unofficial/v6/dropbox/files"
 )
 
 type DropboxSink struct {
@@ -80,7 +80,7 @@ func (s *DropboxSink) CreateObject(ctx context.Context, path string, data io.Rea
 			return err
 		}
 	}
-	arg := dfiles.NewCommitInfo(normalizedPath)
+	arg := dfiles.NewUploadArg(normalizedPath)
 	_, err := dbf.Upload(arg, data)
 	return err
 }
